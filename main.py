dac7c90cb90f87bef1fb1d2dd1f2b2dd86088f83
Michael Slaughter <michaelslaughter77@gmail.com>
2025-12-07 13:55:34 -0600

    add draw_screen function to display 1280 x 720 window of solid black

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    draw_screen()
+
+def draw_screen():
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
