da543ab3e4bd9b1a04d6ab0c9a16d46fd0c14108
Michael Slaughter <michaelslaughter77@gmail.com>
2025-12-07 14:13:58 -0600

    stop using dedicated draw screen function for now

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    draw_screen()
+    #print(type(screen))
+    #draw_screen()
+    while True:
+        #print(type(screen))
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 def draw_screen():
     while True:
+        print(type(screen))
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
