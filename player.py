e056e926c838621926b19c2454ce87a7c26fb932
Michael Slaughter <michaelslaughter77@gmail.com>
2025-12-07 19:57:27 -0600

    update player.py to shoot with more consistent behavior

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cooldown = 0
         
     
         # in the Player class
@@ -25,6 +26,10 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= dt
+            if self.shoot_cooldown < 0:
+                self.shoot_cooldown = 0
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -34,8 +39,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
-            self.shoot()
+        if keys[pygame.K_SPACE] and self.shoot_cooldown == 0:
+                self.shoot()
+                self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
