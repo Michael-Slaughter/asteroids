dcf2b38b5e89096c391e39ad266b5da1209e4402
Michael Slaughter <michaelslaughter77@gmail.com>
2025-12-07 19:41:29 -0600

    add shot.py to make a shot class. update player.py to create a shot at the player's location and add velocity to it

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
-
+from shot import Shot
 
 def main():
     pygame.init()
@@ -21,11 +21,14 @@ def draw_screen():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable,)
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:                                                                 #infinite while loop
         log_state()
 
